7677b5ebd54f7f7d12bc8bf7f926332165b562ca
sumeya <sumeya10@abv.bg>
2024-07-21 21:10:22 +0300

    Fix register

---
+++ src/main/java/com/example/demo/config/MailConfig.java
@@ -0,0 +1,77 @@
+package com.example.demo.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@ConfigurationProperties(prefix = "mail")
+public class MailConfig {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setHost(host);
+        javaMailSender.setPort(port);
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);
+        javaMailSender.setDefaultEncoding("UTF-8");
+        javaMailSender.setJavaMailProperties(mailProperties());
+
+        return javaMailSender;
+    }
+
+    private Properties mailProperties() {
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.transport.protocol", "smtp");
+
+        return properties;
+    }
+
+}
