06e1c63eda17dabb6a63dc73e8aa4db93e822b97
sumeya <sumeya10@abv.bg>
2024-08-07 14:39:55 +0300

    Fix userUpdate controller

--- src/main/java/com/prodavalnik/prodavalnik/web/UpdateUserController.java
+++ src/main/java/com/prodavalnik/prodavalnik/web/UpdateUserController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -32,7 +33,7 @@ public class UpdateUserController {
         return new ModelAndView("update");
     }
 
-    @PutMapping("/users/update")
+    @PostMapping("/users/update")
     public ModelAndView update(@Valid @ModelAttribute("userUpdateInfoDTO") UserUpdateInfoDTO userUpdateInfoDTO,
                                BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
